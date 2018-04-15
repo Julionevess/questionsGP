68a7169c3283a9548c2090b38f61756bd3c1c56c
julio <julionevess@gmail.com>
2018-04-15 20:40:31 -0300

    initial

---
+++ app/src/main/java/com/jns/questoesgp/model/Question.java
@@ -0,0 +1,81 @@
+package com.jns.questoesgp.model;
+
+/**
+ * Created by julio on 15/04/2018.
+ */
+
+public class Question {
+
+    private Long id;
+    private String question;
+    private String answer;
+    private String category;
+    private String optionOne;
+    private String optionTwo;
+    private String optionThree;
+    private String optionFour;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public String getOptionOne() {
+        return optionOne;
+    }
+
+    public void setOptionOne(String optionOne) {
+        this.optionOne = optionOne;
+    }
+
+    public String getOptionTwo() {
+        return optionTwo;
+    }
+
+    public void setOptionTwo(String optionTwo) {
+        this.optionTwo = optionTwo;
+    }
+
+    public String getOptionThree() {
+        return optionThree;
+    }
+
+    public void setOptionThree(String optionThree) {
+        this.optionThree = optionThree;
+    }
+
+    public String getOptionFour() {
+        return optionFour;
+    }
+
+    public void setOptionFour(String optionFour) {
+        this.optionFour = optionFour;
+    }
+}
