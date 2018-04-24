234f260f1fdb127851fb2c953688de3e3aed7c39
julio <julionevess@gmail.com>
2018-04-24 14:16:34 -0300

    ok

--- QuestionGPServer/js/grid.js
+++ QuestionGPServer/js/grid.js
@@ -7,6 +7,19 @@ var incorrectOptionTwo;
 var incorrectOptionThree;
 var incorrectOptionFour;
 
+$(document).ready(function(){
+	$("#search").on("keyup", function() {
+	  var value = $(this).val().toLowerCase();
+	  $("#questionTable tr").filter(function() {
+		$(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+	  });
+	});
+  });
+
+var json = "[{\"id\":\"1\",\"question\":\"Qual o seu nome?\",\"answer\":\"Jose\",\"optionOne\":\"Maria\",\"optionTwo\":\"Fernando\",\"optionThree\":\"Joaquim\",\"optionFour\":\"Paulo\",\"category\":\"Categoria 1\"},{\"id\":\"2\",\"question\":\"Qual a sua idade?\",\"answer\":\"18\",\"optionOne\":\"17\",\"optionTwo\":\"22\",\"optionThree\":\"25\",\"optionFour\":\"33\",\"category\":\"Categoria 1\"},{\"id\":\"3\",\"question\":\"Qual o seu telefone?\",\"answer\":\"81 9.9898-9898\",\"optionOne\":\"11 9.9898-9898\",\"optionTwo\":\"88 9.9898-9898\",\"optionThree\":\"81 9.9898-7898\",\"optionFour\":\"81 9.9898-9897\",\"category\":\"Categoria 1\"}]"
+
+var jsonObject = JSON.parse(json);
+
 function sendForm(){
 	
 	question = $('#question').val();
@@ -76,7 +89,38 @@ $(function() {
 	});
 });
 
+function loadTable(){
+
+	jQuery.each(jsonObject, function (index, value) {
+        if (typeof value == 'object') {
+			var row = $('<tr></tr>');
+			$('#questionTable').append(row);
+			$('<td></td>').attr({ class: ["col-20"]}).text(value.category).appendTo(row);
+			$('<td></td>').attr({ class: ["col-80"]}).text(value.question).appendTo(row);
+            
+        }
+        else {
+             alert(index + "   :   " + value);
+        }
+	});
+	/*
+	for (var prop in jsonObject) {
+		alert("id:" + prop.id);
+		alert("Question:" + prop.question);
+	}
+
+/*
+	for(i=0; i<3; i++){
+		var row = $('<tr></tr>');
+		$('#questionTable').append(row);
+		$('<td></td>').attr({ class: ["col-20"]}).text("Cat" + i).appendTo(row);
+		$('<td></td>').attr({ class: ["col-80"]}).text("Pergunta" + i).appendTo(row);
+		
+	}*/
+}
+
 function init(){
+	loadTable();
 /*
 	var txt = '';
 	var xmlhttp = new XMLHttpRequest();
